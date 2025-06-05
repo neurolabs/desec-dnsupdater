9100286dc2b4c4c18bc7baaca2239131d0c4b154
Ole Langbehn <ole.langbehn@inoio.de>
2025-06-05 13:45:36 +0200

    v0.1.1

--- desec_dyndns.py
+++ desec_dyndns.py
@@ -16,11 +16,35 @@ import netifaces
 import requests  # type: ignore[import-untyped]
 from requests.adapters import HTTPAdapter, Retry  # type: ignore[import-untyped]
 
-# TODO: Allow usage via cron one-off with a click command,
-# add other click command for systemd service
+
+@dataclasses.dataclass
+class Updates:
+    """data class for updates."""
+
+    A: str | None = None
+    AAAA: str | None = None
+
+    def as_tuples(self) -> list[tuple[desec.DnsRecordTypeType, str]]:
+        """Convert the updates to a list of tuples."""
+        result: list[tuple[desec.DnsRecordTypeType, str]] = []
+        if self.A:
+            result.append(("A", self.A))
+        if self.AAAA:
+            result.append(("AAAA", self.AAAA))
+        return result
+
 
 _log_target = sys.stdout
 _log_verbosity_level = 0
+
+
+def _configure_logging(log_target: click.File, verbosity: int) -> None:
+    """Configure logging for the script."""
+    global _log_target, _log_verbosity_level
+    _log_target = log_target
+    _log_verbosity_level = verbosity
+    # verbosity 3 -> level 10 (DEBUG), verbosity 2 -> level 20 (INFO), verbosity 1 -> level 30 (WARNING), verbosity 0 -> level 40 (ERROR)
+    desec._configure_cli_logging(40 - verbosity * 10)
 
 
 def _debug(*args: Any, **kwargs: Any) -> None:
@@ -49,25 +73,6 @@ def _error(*args: Any, **kwargs: Any) -> None:
     click.secho(*args, **kwargs)
 
 
-def _resolve_nameservers(servers: list[str]) -> list[str]:
-    """Resolve the nameservers for deSEC."""
-    result = []
-    for server in servers:
-        with contextlib.suppress(Exception):
-            result.append(socket.gethostbyname(server))
-    if len(result) == 0:
-        _error("No nameserver could be resolved from hardcoded list of deSEC's DNS servers.")
-        raise click.ClickException(
-            "No nameserver could be resolved, neither from domain's NS records nor from hardcoded list of desec's DNS servers."
-        )
-    return result
-
-
-CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"], auto_envvar_prefix="DESEC_DYNDNS")
-
-desec._configure_cli_logging(10)
-
-
 def _get_public_ipv4() -> str | None:
     try:
         s = requests.Session()
@@ -83,14 +88,13 @@ def _get_public_ipv4() -> str | None:
         return None
 
 
-def _mac_to_ipv6_suffix(mac: str) -> str:
-    mac_parts = mac.split(":")
-    first_part_with_seventh_bit_inverted = f"{int(mac_parts[0], 16) ^ 0x02:02x}"
-    mac_parts = [first_part_with_seventh_bit_inverted] + mac_parts[1:3] + ["ff", "fe"] + mac_parts[3:]
-    return ":".join("".join(mac_parts[i : i + 2]) for i in range(0, len(mac_parts), 2))
-
-
 def _get_public_ipv6(interface_name: str) -> str | None:
+    def _mac_to_ipv6_suffix(mac: str) -> str:
+        mac_parts = mac.split(":")
+        first_part_with_seventh_bit_inverted = f"{int(mac_parts[0], 16) ^ 0x02:02x}"
+        mac_parts = [first_part_with_seventh_bit_inverted] + mac_parts[1:3] + ["ff", "fe"] + mac_parts[3:]
+        return ":".join("".join(mac_parts[i : i + 2]) for i in range(0, len(mac_parts), 2))
+
     mac = netifaces.ifaddresses(interface_name).get(netifaces.AF_PACKET)[0].get("addr")  # type: ignore[call-overload]
     mac_as_ipv6_suffix = _mac_to_ipv6_suffix(mac)
     adapters = ifaddr.get_adapters()
@@ -104,89 +108,43 @@ def _get_public_ipv6(interface_name: str) -> str | None:
     return None
 
 
-@dataclasses.dataclass
-class Updates:
-    """data class for updates."""
-
-    A: str | None = None
-    AAAA: str | None = None
-
-    def as_tuples(self) -> list[tuple[desec.DnsRecordTypeType, str]]:
-        """Convert the updates to a list of tuples."""
-        result: list[tuple[desec.DnsRecordTypeType, str]] = []
-        if self.A:
-            result.append(("A", self.A))
-        if self.AAAA:
-            result.append(("AAAA", self.AAAA))
-        return result
-
-
 def _get_resolver_against_domain_nameservers(domain: str) -> dns.resolver.Resolver:
     """Get a DNS resolver configured to use deSEC nameservers."""
+
+    def _resolve_nameservers(servers: list[str]) -> list[str]:
+        """Resolve the nameservers for deSEC."""
+        result = []
+        for server in servers:
+            with contextlib.suppress(Exception):
+                result.append(socket.gethostbyname(server))
+        if len(result) == 0:
+            _error("No nameserver could be resolved from hardcoded list of deSEC's DNS servers.")
+            raise click.ClickException(
+                "No nameserver could be resolved, neither from domain's NS records nor from hardcoded list of desec's DNS servers."
+            )
+        return result
+
     resolver = dns.resolver.Resolver()
     # get nameservers from the domain
     try:
         nameservers = [ns.to_text().strip(".") for ns in resolver.resolve(domain, "NS")]
     except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
-        _info(f"No NS records found for domain {domain}, using default deSEC nameservers.")
+        _warn(f"No NS records found for domain {domain}, using default deSEC nameservers.")
         nameservers = ["ns1.desec.io", "ns2.desec.org"]
         # If no NS records are found, use the default deSEC nameservers
     resolver.nameservers = _resolve_nameservers(nameservers)
     return resolver
 
 
-@click.command(context_settings=CONTEXT_SETTINGS, help="Update DNS settings for a deSEC domain.")
-@click.option("--domain", "-d", required=True, help="The domain to update in.")
-@click.option("--subdomain", "-s", multiple=True, required=True, help="The subdomain(s) to update.")
-@click.option("--token", "-t", required=True, help="The token to use for authentication.")
-@click.option(
-    "--interface",
-    "-i",
-    help="The network interface to use for determining the IPv6 address. If not set, IPv6 is not updated.",
-)
-@click.option(
-    "--wait-time",
-    "-w",
-    type=int,
-    default=5,
-    show_default=True,
-    help="The wait period between domain updates in seconds (for respecting rate limits).",
-)
-@click.option(
-    "--log-file",
-    "-l",
-    type=click.File("w"),
-    default="-",
-    show_default=True,
-    help="The file to write logs to. Defaults to stdout.",
-)
-@click.option(
-    "--verbose",
-    "-v",
-    count=True,
-    help="Increase verbosity of output (can be used multiple times for more verbosity, e.g. `-vvv`). Default is errors only. Once for info, twice or more for debug.",
-)
-@click.option(
-    "--dry-run",
-    is_flag=True,
-    show_default=True,
-    default=False,
-    help="Don't actually update the DNS records, just print what would be done.",
-)
-def update(
+def _update_once(
     domain: str,
     subdomain: list[str],
     token: str,
     interface: str | None,
     wait_time: int,
-    log_file: click.File,
-    verbose: int,
     dry_run: bool,
 ) -> None:
-    """Update DNS settings for a deSEC domain."""
-    global _log_target, _log_verbosity_level
-    _log_target = log_file
-    _log_verbosity_level = verbose
+    """Update the DNS records for the given domain and subdomain."""
     to_update: Updates = Updates()
     # Get the public IPv4 address
     public_ipv4 = _get_public_ipv4()
@@ -230,6 +188,84 @@ def update(
                         domain=domain, subname=subdomain_name, rtype=rtype, rrset=[public_ip], ttl=3600
                     )
                 sleep(wait_time)
+
+
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"], auto_envvar_prefix="DESEC_DYNDNS")
+
+
+@click.command(context_settings=CONTEXT_SETTINGS, help="Update DNS settings for a deSEC domain.")
+@click.option("--domain", "-d", required=True, help="The domain to update in.")
+@click.option("--subdomain", "-s", multiple=True, required=True, help="The subdomain(s) to update.")
+@click.option("--token", "-t", required=True, help="The token to use for authentication.")
+@click.option(
+    "--interface",
+    "-i",
+    help="The network interface to use for determining the IPv6 address. If not set, IPv6 is not updated.",
+)
+@click.option(
+    "--log-file",
+    "-l",
+    type=click.File("w"),
+    default="-",
+    show_default=True,
+    help="The file to write logs to. Defaults to stdout.",
+)
+@click.option(
+    "--verbose",
+    "-v",
+    count=True,
+    help="Increase verbosity of output (can be used multiple times for more verbosity, e.g. `-vvv`). Default is errors only. Once for info, twice or more for debug.",
+)
+@click.option(
+    "--dry-run",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Don't actually update the DNS records, just print what would be done.",
+)
+@click.option(
+    "--continuous",
+    "-c",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Run the update in a loop, waiting for the specified wait time between updates.",
+)
+@click.option(
+    "--wait-time-between-checks",
+    type=int,
+    default=60,
+    show_default=True,
+    help="The wait time between checks for updates in seconds (for continuous updates, see --continuous/-c).",
+)
+@click.option(
+    "--wait-time-between-api-calls",
+    type=int,
+    default=5,
+    show_default=True,
+    help="The wait time between domain update api calls in seconds (for respecting rate limits).",
+)
+def update(
+    domain: str,
+    subdomain: list[str],
+    token: str,
+    interface: str | None,
+    log_file: click.File,
+    verbose: int,
+    dry_run: bool,
+    continuous: bool,
+    wait_time_between_checks: int,
+    wait_time_between_api_calls: int,
+) -> None:
+    """Update DNS settings for a deSEC domain."""
+    _configure_logging(log_file, verbose)
+    if continuous:
+        _debug(f"Running in continuous mode, will update every {wait_time_between_checks} seconds.")
+        while True:
+            _update_once(domain, subdomain, token, interface, wait_time_between_api_calls, dry_run)
+            sleep(wait_time_between_checks)
+    else:
+        _update_once(domain, subdomain, token, interface, wait_time_between_api_calls, dry_run)
 
 
 if getattr(sys, "frozen", False):
